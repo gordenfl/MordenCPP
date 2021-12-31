f7d53b170c4cbcf7fd062413bf8b41f9fea802b1
gordenfl <gordenfl@gmail.com>
2021-12-30 18:19:12 -0800

    change project ISO C++ stander

--- MordenCPP/StructedBinding.cpp
+++ MordenCPP/StructedBinding.cpp
@@ -15,10 +15,12 @@ void beforeBind() {
 }
 
 void afterBind() {
-	//std::vector<std::pair<int, float>> data{ {1,1.2}, {3,3.14}, {2, 2.2} };
-	std::pair<int, float> data{1, 3.2};
-	//const auto& [x, y] = data;
-	std::cout << std::endl;
+	std::vector<std::pair<int, float>> data{ {1,1.2}, {3,3.14}, {2, 2.2} };
+
+	for (auto v : data) {
+		const auto& [a, b] = v;
+		std::cout << a << "->" << b << std::endl;
+	}
 }
 
 void StructuredBind_Test() {
