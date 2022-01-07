546db4eb0ab9c91605c8db1ae1ee93d3f415d256
gordenfl <gordenfl@gmail.com>
2022-01-07 01:29:00 -0800

    charactor format

--- MordenCPP/CPPCoreGuidelines.cpp
+++ MordenCPP/CPPCoreGuidelines.cpp
@@ -5,7 +5,6 @@
 #include<vector>
 #include<string>
 #include<span>
-#include<chrono>
 
 using std::vector;
 using std::string;
@@ -17,7 +16,7 @@ Month Date::month() const {
 	return _month;
 }
 
-//for_each �� algorithm ͷ�ļ�� ������Ӵ�һ��lambda
+//for_each 在 algorithm 头文件里， 这个例子带一个lambda
 void ExampleForEach() {
 	std::vector<int> data{ 1,2,3,4,5,6,7,8,9 };
 	for_each(begin(data), end(data), [](int v) {
@@ -26,9 +25,9 @@ void ExampleForEach() {
 	std::cout << std::endl;
 }
 
-void Find(vector<string>& data, string & target) {
+void Find(vector<string>& data, string& target) {
 	auto p = find(begin(data), end(data), target); //return is an iterator
-	if (p!=data.end()) {
+	if (p != data.end()) {
 		std::cout << "Find the target:" << *p << std::endl;
 	}
 	else {
@@ -36,32 +35,33 @@ void Find(vector<string>& data, string & target) {
 	}
 }
 
+
 void Cpp_Core_Test() {
 	cout << "||For: for_each learning" << endl;
 	ExampleForEach();
 
 	cout << "||For: find learning" << endl;
-	auto data = std::vector<std::string>{ "AAA", "BBB", "CCC", "DDD", "target"};
+	auto data = vector<string>{ "AAA", "BBB", "CCC", "DDD", "target" };
 	auto target = string("BBB");
 	Find(data, target);
 
 
 	// not_null,  code from:https://github.com/microsoft/GSL/blob/main/include/gsl/pointers
-	//�Կ�ָ�뱣�����Լ������not_null��ģ���࣬���������Ѻ�
+	//对空指针保护，自己定义个not_null的模板类，对这样更加友好
 
 
 	//Do not pass an array as a single pointer
-	//���������span ģ�彫�����װһ�Σ�������Ա�֤����Խ����ʣ�����ᰲȫ�����һ����һ���ʾ��С�Ĳ���
+	//这里最好用span 模板将数组包装一次，这样可以保证不会越界访问，这样会安全，并且会减少一个表示大小的参数
 
 
-	//���⸴�ӵĳ�ʼ����������Ҫ��ĳ�������ܳ�ʼ�����࣬�ĳɼ򵥳�ʼ������ĳ������ڶ����ʼ���Ժ����ĳ�����ʼ����ȥ
+	//避免复杂的初始化，比如需要传某个参数才能初始化的类，改成简单初始化，将某个参数在对象初始化以后调用某个函数初始化进去
 	// file1.cpp
 	// extern const X x;
 	//const Y y = fun(x);
 	// file2.cpp
 	// extern const Y y;
 	// const X x = func(y);
-	//����������ѭ���༭����
+	//这里会出现死循环编辑错误
 	/*
 	file1.cpp
 	extern const X;
@@ -75,22 +75,22 @@ void Cpp_Core_Test() {
 	*/
 
 
-	//������ٺ������ĸ����ø��ַ�����
-	//��struct class ��װ�������ĳ�Ա������õȵ�
+	//尽量减少函数参数的个数，用各种方法：
+	//用struct class 包装，类对象的成员变量设置等等
 
-	//�������ڵĲ���ɲ�ͬ����Ĳ���ʹ��
-	//����:  void copy_n(T* p, T* q, int n); //copy from p to q, copy length is n. this is bad
+	//避免相邻的参数被当成不同含义的参数使用
+	//例子:  void copy_n(T* p, T* q, int n); //copy from p to q, copy length is n. this is bad
 	// void copy_n(const T* p, T* q, int n); //const of p, then p will not be used as target point
 	// void copy_n(span<const T> p, span<T> q);  // this is good
 
 
-	//�����ྡ�ֻ����ӿڣ���Ҫ����ݣ���Ա����ȵ�
+	//抽象类尽量只定义接口，不要包含数据，成员变量等等
 
 
-	//�����Ҫ������루��һ��ƽ̨�ϱ���������һ��ƽ̨�����еĳ��򣩣������C-Style �Ӽ�
+	//如果需要交叉编译（在一个平台上编译在另外一个平台上运行的程序），最好用C-Style 子集
 
 
-	//����������ԭ����Ҫ�õ�һЩ�Ƚϳ�ª�Ĵ���Ͳ���ȫ�Ĵ��룬�뽫���뱣�����ڲ�����Ҫ��Ⱦ�ӿ�
+	//如果出于特殊原因需要用到一些比较丑陋的代码和不安全的代码，请将代码保留在内部，不要感染接口
 
 
 }
